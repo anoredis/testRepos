511a2ca00760028036ffb14c92d92e4b310d8026
Christos Kyriazis <christos.kyriazis@live.com>
2023-06-06 15:36:42 +0300

    full project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Test</h1>
+      <hr with="100%" />
+      this is just a test
+    </>
+  );
+}
+
+export default App;
